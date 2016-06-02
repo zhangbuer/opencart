4669f0bd959fea82bccc4847b83b6f2b580c7966
runphp <runphp@qq.com>
2016-06-02 17:51:21 +0800

    添加简体中文

---
+++ admin/language/zh-CN/localisation/language.php
@@ -0,0 +1,35 @@
+<?php
+
+// Heading
+$_['heading_title']     = '语言';
+
+// Text
+$_['text_success']      = '成功: 已修改语言！';
+$_['text_list']         = '语言列表';
+$_['text_add']          = '添加语言';
+$_['text_edit']         = '编辑语言';
+
+// Column
+$_['column_name']       = '语言名称';
+$_['column_code']       = '代码';
+$_['column_sort_order'] = '排序';
+$_['column_action']     = '操作';
+
+// Entry
+$_['entry_name']        = '语言名称';
+$_['entry_code']        = '代码';
+$_['entry_status']      = '状态';
+$_['entry_sort_order']  = '排序';
+
+// Help
+$_['help_status']       = '在语言下拉项中隐藏/显示';
+
+// Error
+$_['error_permission']  = '警告: 无权限修改语言！';
+$_['error_exists']      = '警告: 已存在！';
+$_['error_name']        = '语言名称必须为3-32字符！';
+$_['error_code']        = '语言代码至少为2个字符！';
+$_['error_default']     = '警告: 不能删除此语言，该语言已经被关联为商店前台默认语言！';
+$_['error_admin']       = '警告: 不能删除此语言，该语言已经被关联为网站后台默认语言！';
+$_['error_store']       = '警告: 不能删除此语言，该语言已经被关联到 %s 商店！';
+$_['error_order']       = '警告: 不能删除此语言，该语言已经被关联到 %s 个订单！';
