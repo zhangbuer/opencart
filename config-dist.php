da9eb61490b2516250072dd14f3cf2ad554a2099
runphp <runphp@qq.com>
2016-12-07 08:41:19 +0800

    add config example add cart css

---
+++ config-dist.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_LANGUAGE', '/var/www/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_CACHE', '/var/www/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
